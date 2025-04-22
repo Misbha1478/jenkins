ad64935ab38e50c89a06b7dd5718428560b9e1c2
Misbha1478 <misbha645@gmail.com>
2025-04-22 05:51:19 -0400

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,8 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        String str = "Java";
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println("Reversed String: " + reversed);
+    }
+}
+
