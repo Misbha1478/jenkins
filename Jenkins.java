a42b98fbfec8c7106202e76e86a49dc25ec76aac
Misbha1478 <misbha645@gmail.com>
2025-04-22 05:03:15 -0400

    Add files via upload
    initial

---
+++ Jenkins.java
@@ -0,0 +1,32 @@
+
+
+class Student {
+    String name;
+    int rollNumber;
+    String course;
+
+    // Constructor to initialize student details
+    Student(String name, int rollNumber, String course) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.course = course;
+    }
+
+    // Method to display student details
+    void displayDetails() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Course: " + course);
+    }
+}
+
+public class Jenkins {
+    public static void main(String[] args) {
+
+     Student s = new Student("Misbha",89,"Devops");
+     s.displayDetails();
+        
+    }
+}
+
+
