1d2820b47742cb80f0f9c68aea52ef4359569d87
Misbha1478 <misbha645@gmail.com>
2025-04-22 05:44:43 -0400

    Add files via upload

---
+++ NaturalNumbers.java
@@ -0,0 +1,8 @@
+public class NaturalNumbers {
+    public static void main(String[] args) {
+        int n = 10; // First 10 natural numbers
+        int sum = (n * (n + 1)) / 2;
+        System.out.println("Sum of first " + n + " natural numbers: " + sum);
+    }
+}
+
