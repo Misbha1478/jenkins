550050ed8d58b0e9bb4feff2c6b75b6d7f2bdf60
Misbha1478 <misbha645@gmail.com>
2025-04-22 05:51:00 -0400

    Add files via upload

---
+++ PrimeNumberCheck.java
@@ -0,0 +1,14 @@
+public class PrimeNumberCheck {
+    public static void main(String[] args) {
+        int num = 29;
+        boolean isPrime = true;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        System.out.println(num + " is " + (isPrime ? "prime" : "not prime"));
+    }
+}
+
